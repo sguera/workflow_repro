2ce0020e792e09324d6c823e2f04428a29720ab8
Antonio Maffia <maffia@cs-austin.cs.unibas.ch>
2014-06-10 14:50:02 +0200

    Cleaned unnecessary files

--- wave/patus/patusGenerated/tuned_params.h
+++
@@ -1,5 +0,0 @@
-#define WAVE_CB_X 0
-#define WAVE_CB_Y 0
-#define WAVE_CB_Z 0
-#define WAVE_CHUNK 0
-#define WAVE__UNROLL_P3 0
--- wave/pluto/compile/compile-pilatus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-module load PrgEnv-intel
-icc -O3 -unroll-aggressive -opt-prefetch -openmp lb-16-16-16-1000.c -o wavepluto.icc.pilatus
-module switch PrgEnv-intel PrgEnv-gnu
-gcc -O3 -ffast-math -funroll-loops -fopenmp -lm lb-16-16-16-1000.c -o wavepluto.gnu.pilatus 
-cp wavepluto.icc.pilatus $SCRATCH
-cp wavepluto.gnu.pilatus $SCRATCH
