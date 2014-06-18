92d5bdbe82b8a49a3b3df109a5e763ad5be76503
sguera <sguera@users.noreply.github.com>
2014-06-18 12:14:08 +0200

    Update installpatus.sh

--- StencilCompilers/patus/installpatus.sh
+++ StencilCompilers/patus/installpatus.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-version=patus-0.1.4
+version=patus-0.1.3
 #check if version is already downloaded and delete it if it is
 if [ -a "$version.zip" ]; then
     rm $version.zip;
 fi
 
-#Downlaod Patus
-wget https://github.com/sguera/Patus/blob/master/$version.zip
+#Downlaod Patus from main repository
+wget http://patus.googlecode.com/files/$version.zip
+#wget https://github.com/sguera/Patus/blob/master/$version.zip
 
 #Extract Patus
 if [ -d "patus" ]; then
